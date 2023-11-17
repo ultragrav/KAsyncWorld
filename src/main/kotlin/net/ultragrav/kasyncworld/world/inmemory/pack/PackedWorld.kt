fcb71c87e8d39a7bf736742102e9420a34368411
UltraDev <petzmagnus@gmail.com>
2023-11-17 09:23:13 +0100

    Add chunk ranges to PackedWorld

--- src/main/kotlin/net/ultragrav/kasyncworld/world/inmemory/pack/PackedWorld.kt
+++ src/main/kotlin/net/ultragrav/kasyncworld/world/inmemory/pack/PackedWorld.kt
@@ -7,4 +7,7 @@ data class PackedWorld(val chunks: List<EncodedAsyncChunk>) {
     val chunkMinZ get() = chunks.minOf { it.z }
     val chunkMaxX get() = chunks.maxOf { it.x }
     val chunkMaxZ get() = chunks.maxOf { it.z }
+
+    val chunkRangeX get() = chunkMinX..chunkMaxX
+    val chunkRangeZ get() = chunkMinZ..chunkMaxZ
 }
