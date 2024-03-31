3f59bf264bd3634ea9a8379b5985e18eaac81ea4
Matthew Lack <matt.lack@mail.utoronto.ca>
2024-03-31 15:19:36 -0400

    Re implement parallel import blocks with getChunk outside of async portion so no deadlocks

--- src/main/kotlin/net/ultragrav/kasyncworld/world/schematic/Schematics.kt
+++ src/main/kotlin/net/ultragrav/kasyncworld/world/schematic/Schematics.kt
@@ -1,5 +1,7 @@
 package net.ultragrav.kasyncworld.world.schematic
 
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Runnable
 import net.minecraft.world.level.ChunkPos
 import net.minecraft.world.phys.Vec3
 import net.ultragrav.kasyncworld.AW
@@ -119,41 +121,29 @@ object Schematics : SchematicsApi {
         val chunkRangeZ = (region.min.z shr 4)..(region.max.z shr 4)
         val sectionRangeY = (region.min.y shr 4)..(region.max.y shr 4)
 
-//        runBlocking(AW.parallelDispatcher) {
-//
-//            val waitFor = mutableListOf<Deferred<Runnable>>()
-//            for (cx in chunkRangeX) {
-//                for (cz in chunkRangeZ) {
-//                    val runnable: Deferred<Runnable> = async {
-//                        val bukkitChunk = world.getChunkAt(cx, cz)
-//                        val chunk = AW.chunkIO.readChunk(
-//                            bukkitChunk,
-//                            factory,
-//                            readOptions.copy(sectionMask = sectionRangeY)
-//                        )
-//
-//                        Runnable {
-//                            aw.setChunk(cx, cz, chunk)
-//                        }
-//                    }
-//                    waitFor.add(runnable)
-//                }
-//            }
-//
-//            waitFor.awaitAll().forEach { it.run() }
-//
-//        }
+        runBlocking(AW.parallelDispatcher) {
 
-        for (cx in chunkRangeX) {
-            for (cz in chunkRangeZ) {
-                val bukkitChunk = world.getChunkAt(cx, cz)
-                val chunk = AW.chunkIO.readChunk(
-                    bukkitChunk,
-                    factory,
-                    readOptions.copy(sectionMask = sectionRangeY)
-                )
-                aw.setChunk(cx, cz, chunk)
+            val waitFor = mutableListOf<Deferred<Runnable>>()
+            for (cx in chunkRangeX) {
+                for (cz in chunkRangeZ) {
+                    val bukkitChunk = world.getChunkAt(cx, cz)
+                    val runnable: Deferred<Runnable> = async {
+                        val chunk = AW.chunkIO.readChunk(
+                            bukkitChunk,
+                            factory,
+                            readOptions.copy(sectionMask = sectionRangeY)
+                        )
+
+                        Runnable {
+                            aw.setChunk(cx, cz, chunk)
+                        }
+                    }
+                    waitFor.add(runnable)
+                }
             }
+
+            waitFor.awaitAll().forEach { it.run() }
+
         }
 
         return aw
